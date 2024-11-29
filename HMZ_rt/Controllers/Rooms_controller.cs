3f11765acc206f8911b13bc667fc6607c2054517
MrTahil <90681023+MrTahil@users.noreply.github.com>
2024-11-29 13:48:14 +0100

    Update Rooms_controller.cs

--- HMZ_rt/Controllers/Rooms_controller.cs
+++ HMZ_rt/Controllers/Rooms_controller.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using HMZ_rt.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HMZ_rt.Controllers
 {
+    [Route("Rooms")]
+    [ApiController]
     public class Rooms_controller : Controller
     {
-        public IActionResult Index()
+        private readonly HmzRtContext _context;
+
+        [HttpGet("SzobaInfokFőképhez")]
+        public async Task<ActionResult<Rooms_controller>> BasesiteofRooms()
         {
-            return View();
+            return Ok( _context.Rooms.ToListAsync());
         }
     }
 }
