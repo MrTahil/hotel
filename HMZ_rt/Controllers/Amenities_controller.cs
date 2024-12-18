3fa744dbf2138d4755bbd0091adf7b9899c7c5c7
MrTahil <Hiloczkit@kkszki.hu>
2024-12-18 13:40:23 +0100

    Auto increment set up

--- HMZ_rt/Controllers/Amenities_controller.cs
+++ HMZ_rt/Controllers/Amenities_controller.cs
@@ -8,64 +8,6 @@ namespace HMZ_rt.Controllers
     [ApiController]
     public class Amenities_controller : Controller
     {
-        private readonly HmzRtContext _context;
-        public Amenities_controller(HmzRtContext context) { _context = context; }
-
-
-        // A collection to keep track of already generated IDs
-        private static HashSet<string> generatedIds = new HashSet<string>();
-        // Method to generate a unique numeric ID
-        public string GenerateUniqueId()
-        {
-            if (generatedIds == null)
-            {
-                var ids = _context.Amenities
-                       .Select(u => u.AmenityId.ToString())
-                       .ToList();
-
-                lock (generatedIds)
-                {
-                    foreach (var id in ids)
-                    {
-                        generatedIds.Add(id);
-                    }
-                }
-            }
-            string uniqueId;
-            do
-            {
-
-                uniqueId = GenerateNumericId();
-            }
-            while (generatedIds.Contains(uniqueId)); // Check if the ID already exists
-
-
-            generatedIds.Add(uniqueId);
-            return uniqueId;
-        }
-        // Method to generate a random numeric ID
-        public static string GenerateNumericId()
-        {
-            // Combine current timestamp (e.g., the number of seconds since Unix epoch) and random digits
-            string timestamp = DateTime.UtcNow.Ticks.ToString().Substring(10); // Take the last 10 digits of the timestamp (for 11 digit ID)
-            string randomPart = GenerateRandomNumericString(1); // Add 1 random digit to make the ID 11 characters long
-            string id = timestamp + randomPart;
-            return id.Length > 11 ? id.Substring(0, 11) : id;
-        }
-        // Method to generate a random numeric string of a specified length
-        private static string GenerateRandomNumericString(int length)
-        {
-            Random random = new Random();
-            string result = string.Empty;
-            for (int i = 0; i < length; i++)
-            {
-                result += random.Next(0, 10);
-            }
-            return result;
-        }
-
-
-
 
         //TODO !!!!!
         [HttpPost("New aminiti{Amenity}")]
--- HMZ_rt/Controllers/UserAccounts_controller.cs
+++ HMZ_rt/Controllers/UserAccounts_controller.cs
@@ -19,72 +19,6 @@ namespace HMZ_rt.Controllers
 
 
 
-
-
-
-
-
-
-
-        // A collection to keep track of already generated IDs
-        private static HashSet<string> generatedIds = new HashSet<string>();
-        // Method to generate a unique numeric ID
-        public string GenerateUniqueId()
-        {
-            if (generatedIds==null)
-            {
-                var ids = _context.Useraccounts
-                       .Select(u => u.UserId.ToString())
-                       .ToList();
-
-                lock (generatedIds)
-                {
-                    foreach (var id in ids)
-                    {
-                        generatedIds.Add(id);
-                    }
-                }
-            }
-            string uniqueId;
-            do
-            {
-
-                uniqueId = GenerateNumericId();
-            }
-            while (generatedIds.Contains(uniqueId)); // Check if the ID already exists
-
-
-            generatedIds.Add(uniqueId);
-            return uniqueId;
-        }
-        // Method to generate a random numeric ID
-        public static string GenerateNumericId()
-        {
-            // Combine current timestamp (e.g., the number of seconds since Unix epoch) and random digits
-            string timestamp = DateTime.UtcNow.Ticks.ToString().Substring(10); // Take the last 10 digits of the timestamp (for 11 digit ID)
-            string randomPart = GenerateRandomNumericString(1); // Add 1 random digit to make the ID 11 characters long
-            string id = timestamp + randomPart; 
-            return id.Length > 11 ? id.Substring(0, 11) : id;
-        }
-        // Method to generate a random numeric string of a specified length
-        private static string GenerateRandomNumericString(int length)
-        {
-            Random random = new Random();
-            string result = string.Empty;
-            for (int i = 0; i < length; i++)
-            {
-                result += random.Next(0, 10);
-            }
-            return result;
-        }
-
-
-
-
-
-
-
-
         [HttpGet("listoutallusers")]
         public async Task<ActionResult<Useraccount>> Userslist()
         {
@@ -122,7 +56,6 @@ namespace HMZ_rt.Controllers
             var user = new Useraccount
             {
                 Username = newuser.UserName,
-                UserId = Convert.ToInt32(GenerateUniqueId()),
                 Password = newuser.Password,
                 Email = newuser.Email,
                 Role = "Base",
