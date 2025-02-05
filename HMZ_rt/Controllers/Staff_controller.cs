043da35916baeaa533ee07497744184e19124441
MrTahil <90681023+MrTahil@users.noreply.github.com>
2025-02-05 13:55:55 +0100

    Staff upload with authorization is done

--- HMZ_rt/Controllers/Staff_controller.cs
+++ HMZ_rt/Controllers/Staff_controller.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using HMZ_rt.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HMZ_rt.Controllers
 {
+    [Route("Staff")]
+    [ApiController]
     public class Staff_controller : Controller
     {
-        public IActionResult Index()
+        private readonly HmzRtContext _context;
+        public Staff_controller(HmzRtContext context)
         {
-            return View();
+            _context = context;
+        }
+
+
+
+        [Authorize(Roles = "System,Admin")]
+        [HttpPost]
+        public async Task<ActionResult<Staff>> CreateStaff(NewStaffDto nwstdto)
+        {
+            var dolgozo = new Staff
+            {
+                FirstName = nwstdto.FirstName,
+                LastName = nwstdto.LastName,
+                Email = nwstdto.Email,
+                PhoneNumber = nwstdto.PhoneNumber,
+                Position = nwstdto.Position,
+                Salary = nwstdto.Salary,
+                DateHired = DateTime.Now,
+                Status = nwstdto.Status,
+                Department = nwstdto.Departmen
+            };
+            if (nwstdto != null)
+            {
+                await _context.Staff.AddAsync(dolgozo);
+                await _context.SaveChangesAsync();
+                return StatusCode(201, dolgozo);
+            }
+            return BadRequest();
         }
     }
 }
