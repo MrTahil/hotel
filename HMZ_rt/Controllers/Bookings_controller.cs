34d65840e28921c4fcea05e965389b7e58da95fa
MrTahil <90681023+MrTahil@users.noreply.github.com>
2025-02-05 13:15:27 +0100

    now u can make booking if ur logged in

--- HMZ_rt/Controllers/Bookings_controller.cs
+++ HMZ_rt/Controllers/Bookings_controller.cs
@@ -1,12 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using HMZ_rt.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HMZ_rt.Controllers
 {
+    [Route("Bookings")]
+    [ApiController]
     public class Bookings_controller : Controller
     {
-        public IActionResult Index()
+        private readonly HmzRtContext _context;
+        public Bookings_controller(HmzRtContext context)
         {
-            return View();
+            _context = context;
         }
+
+        [Authorize(Roles ="Base")]
+        [HttpPost("New_Booking{RoomId}")]
+        public async Task<ActionResult<Booking>> Booking(int roomid, CreateBookingDto crtbooking)
+        {
+            var booking = new Booking
+            {
+                CheckInDate = crtbooking.CheckInDate,
+                CheckOutDate = crtbooking.CheckOutDate,
+                GuestId = crtbooking.GuestId,
+                RoomId = roomid,
+                TotalPrice = crtbooking.TotalPrice,
+                BookingDate = DateTime.Now,
+                PaymentStatus = crtbooking.PaymentStatus,
+                NumberOfGuests = crtbooking.NumberOfGuests
+            };
+            if (crtbooking != null) { 
+            await _context.Bookings.AddAsync(booking);
+                await _context.SaveChangesAsync();
+                return StatusCode(201, booking);
+                
+            }
+            return BadRequest();
+        }
+        
+
+
+
+
     }
 }
