df7332247ad1a33b8f79186dfea38fba84b3eede
MrTahil <90681023+MrTahil@users.noreply.github.com>
2025-02-28 13:28:48 +0100

    Jsonignore added

--- HMZ_rt/Models/Promotion.cs
+++ HMZ_rt/Models/Promotion.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace HMZ_rt.Models;
 
@@ -24,6 +25,6 @@ public partial class Promotion
     public string? Status { get; set; }
 
     public DateTime? DateAdded { get; set; }
-
+    [JsonIgnore]
     public virtual Room? Room { get; set; }
 }
