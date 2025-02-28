3e0cf60c8c2d8d45f62f305da43caee0a1e3a7fb
MrTahil <90681023+MrTahil@users.noreply.github.com>
2025-02-28 12:59:21 +0100

    Promotions get added

--- HMZ_rt/Controllers/Promotions_controller.cs
+++ HMZ_rt/Controllers/Promotions_controller.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using HMZ_rt.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HMZ_rt.Controllers
 {
+    [Route("Promotions")]
+    [ApiController]
     public class Promotions_controller : Controller
     {
-        public IActionResult Index()
+        private readonly HmzRtContext _context;
+        public Promotions_controller(HmzRtContext context)
         {
-            return View();
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<Promotion>> getpromotions()
+        {
+            try
+            {
+                var promotions = await _context.Promotions.ToListAsync();
+                return StatusCode(201, promotions);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, ex);
+            }
         }
     }
 }
