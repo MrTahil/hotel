63c3ce909fe72955a1c337655c72c1feefb8f974
MrTahil <90681023+MrTahil@users.noreply.github.com>
2025-02-28 13:23:56 +0100

    Now u can update promotions

--- HMZ_rt/Controllers/Promotions_controller.cs
+++ HMZ_rt/Controllers/Promotions_controller.cs
@@ -1,4 +1,5 @@
 using HMZ_rt.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -27,6 +28,55 @@ namespace HMZ_rt.Controllers
             {
 
                 return StatusCode(500, ex);
+            }
+        }
+        [Authorize(Roles = "System,Admin,Recept")]
+        [HttpDelete("Deletepromotion/{id}")]
+        public async Task<ActionResult<Promotion>> DeletePromotion(int id)
+        {
+            try
+            {
+                var promotion =await _context.Promotions.FirstOrDefaultAsync(x => x.PromotionId == id);
+                if (promotion != null)
+                {
+                     _context.Promotions.Remove(promotion);
+                    await _context.SaveChangesAsync();
+                    return StatusCode(201, "Sikeres törlés");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, ex);
+            }
+        }
+
+        [Authorize(Roles = "System,Admin,Recept")]
+        [HttpPut("UpdatePromotion/{id}")]
+        public async Task<ActionResult<Promotion>> UpdateRoom(int id, promotionupdate udto)
+        {
+            try
+            {
+                var os = await _context.Promotions.FirstOrDefaultAsync(x => x.PromotionId == id);
+                if (os != null)
+                {
+                    os.PromotionName = udto.Name;
+                    os.Description = udto.Description;
+                    os.StartDate = udto.StartDate;
+                    os.EndDate = udto.EndDate;
+                    os.DiscountPercentage = udto.DiscountPercentage;
+                    os.TermsConditions = udto.TermsConditions;
+                    os.Status = udto.Status;
+                    await _context.SaveChangesAsync();
+                    return StatusCode(201, "Sikeres mentés");
+                }
+                return StatusCode(404);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+                throw;
             }
         }
     }
