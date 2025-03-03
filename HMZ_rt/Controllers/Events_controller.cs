e248c782995385185286fe51de00d64859bee9ed
MrTahil <90681023+MrTahil@users.noreply.github.com>
2025-03-03 08:42:21 +0100

    Events admin get added

--- HMZ_rt/Controllers/Events_controller.cs
+++ HMZ_rt/Controllers/Events_controller.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using HMZ_rt.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HMZ_rt.Controllers
 {
+    [Route("Events")]
+    [ApiController]
     public class Events_controller : Controller
     {
-        public IActionResult Index()
+        private readonly HmzRtContext _context;
+        public Events_controller(HmzRtContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [Authorize(Roles = "Admin, System,Admin")]
+        [HttpGet("Geteventsadmin")]
+        public async Task<ActionResult<Event>> Getallevent()
+        {
+            try
+            {
+                var events = await _context.Events.Include(x => x.Eventbookings).ToListAsync();
+                if (events != null) {
+                    return StatusCode(201, events);
+
+                }
+                return StatusCode(404, "üres");
+
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, ex);
+            }
         }
     }
 }
