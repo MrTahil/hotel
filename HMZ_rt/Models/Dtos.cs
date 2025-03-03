def5059fc33311f63dc0450ebde15a664edcb2a6
MrTahil <90681023+MrTahil@users.noreply.github.com>
2025-03-03 09:55:29 +0100

    Event update dto added

--- HMZ_rt/Models/Dtos.cs
+++ HMZ_rt/Models/Dtos.cs
@@ -219,3 +219,22 @@ public class CreateEvent
     [Required(ErrorMessage = "A kontaktinfó megadása kötelező")]
     public string? ContactInfo { get; set; }
 }
+public class UpdateEvent
+{
+    [Required(ErrorMessage = "Kapacitás megadása kötelező!")]
+    public int? Capacity { get; set; }
+    [Required(ErrorMessage = "A státusz megadása kötelező!")]
+    public string? Status { get; set; }
+    [Required(ErrorMessage = "Az esemény nem szerepelhet név nélkül")]
+    public string? EventName { get; set; }
+    [Required(ErrorMessage = "Az esemény időpontjának megadásakötelező!")]
+    public DateTime? EventDate { get; set; }
+    [Required(ErrorMessage = "A hely megadása kötelező!")]
+    public string? Location { get; set; }
+    [Required(ErrorMessage = "A leírás megadásakötelező!")]
+    public string? Description { get; set; }
+    [Required(ErrorMessage = "Szervező nevének megadása kötelező!")]
+    public string? OrganizerName { get; set; }
+    [Required(ErrorMessage = "A kontaktinfó megadása kötelező")]
+    public string? ContactInfo { get; set; }
+}
