a9a350fd40bd8ed46d64bf9d9234eabca0e442fe
monostorir <90681683+aBeto56@users.noreply.github.com>
2025-04-06 16:12:17 +0200

    New Dto for newsletter implementation

---
+++ hmz_rtWPF/hmz_rt/Models/Dtos/NewsLetter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hmz_rt.Models.Dtos
+{
+    public class NewsletterDto
+    {
+        public string Subject { get; set; }
+        public string HtmlBody { get; set; }
+    }
+}
