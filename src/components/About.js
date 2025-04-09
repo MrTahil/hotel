74a14540e3cba1a3dddd26b2995bfdea0bf48c3a
ZuZuvagyok <zelenakz@kkszki.hu>
2025-04-09 09:42:18 +0200

    location fx

--- src/components/About.js
+++ src/components/About.js
@@ -2,30 +2,16 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function About() {
-    const [hotelLocation, setHotelLocation] = useState(null);
-
-    useEffect(() => {
-        axios.get('/api/hotel-location')
-            .then(response => {
-                setHotelLocation(response.data);
-            })
-            .catch(error => {
-                console.error("Hiba a hotel adatainak lekérésekor:", error);
-            });
-    }, []);
+    const [hotelLocation, setHotelLocation] = useState({
+        lat: 48.10544809701992,
+        lng: 20.77997524506268,
+        address: "Miskolc, Széchenyi utca 35, 3525"
+    });
 
     const openGoogleMaps = () => {
         if (!hotelLocation) return;
         window.open(`https://www.google.com/maps/dir/?api=1&destination=${hotelLocation.lat},${hotelLocation.lng}&travelmode=driving`, '_blank');
     };
-
-    if (!hotelLocation) {
-        return (
-            <div className="text-center py-16 text-xl text-gray-500">
-                Betöltés folyamatban...
-            </div>
-        );
-    }
 
     return (
         <section className="relative py-16 bg-gradient-to-b from-blue-50 to-white">
